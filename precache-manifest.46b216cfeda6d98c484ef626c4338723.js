64b44504554ad948008a585cb9139c9bd5398af3
haseebsheikh417 <sheikhabdulhaseebb417@gmai.com>
2020-08-15 04:53:07 -0700

    Updates

---
+++ precache-manifest.46b216cfeda6d98c484ef626c4338723.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eba2b84fbdb403d25ecf77002c8fcb21",
+    "url": "/Parhako-Challenge-1/index.html"
+  },
+  {
+    "revision": "134d10f570b359817e73",
+    "url": "/Parhako-Challenge-1/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "dfb529fa07416a09d471",
+    "url": "/Parhako-Challenge-1/static/js/2.ea6759e1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Parhako-Challenge-1/static/js/2.ea6759e1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "134d10f570b359817e73",
+    "url": "/Parhako-Challenge-1/static/js/main.802b9d06.chunk.js"
+  },
+  {
+    "revision": "a0e0e8b11d919ffa530f",
+    "url": "/Parhako-Challenge-1/static/js/runtime-main.6d1dae55.js"
+  }
+]);
